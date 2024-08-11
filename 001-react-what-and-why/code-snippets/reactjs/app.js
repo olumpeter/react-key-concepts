b92f367e1dda2d7a5f5a1f38ba848c4622c062f8
Peter Olum <44267527+olumpeter@users.noreply.github.com>
2024-08-11 05:43:21 +0300

    Add files via upload

---
+++ 001-react-what-and-why/code-snippets/reactjs/app.js
@@ -0,0 +1,23 @@
+/* 
+Please note: This snippet is not executable - it's not part of an overall 
+React project. 
+*/
+
+import { useState } from 'react';
+
+function App() {
+    const [outputText, setOutputText] = useState(false);
+
+    function updateTextHandler() {
+        setOutputText('Text was changed!');
+    }
+
+    return (
+        <>
+            <button onClick={updateTextHandler}>Click to change text</button>
+            <p>{outputText}</p>
+        </>
+    );
+}
+
+export default App;
