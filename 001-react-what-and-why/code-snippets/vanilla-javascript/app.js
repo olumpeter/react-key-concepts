6ed0b0635a0266cfea090d45fe8c8e60a860c320
Peter Olum <44267527+olumpeter@users.noreply.github.com>
2024-08-15 19:38:40 +0300

    del

--- 001-react-what-and-why/code-snippets/vanilla-javascript/app.js
+++
@@ -1,8 +0,0 @@
-const buttonElement = document.querySelector("button");
-const paragraphElement = document.querySelector("p");
-
-function updateTextHandler() {
-    paragraphElement.textContent = "Text was changed!";
-}
-
-buttonElement.addEventListener('click', updateTextHandler);
